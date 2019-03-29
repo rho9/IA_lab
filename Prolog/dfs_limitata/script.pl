d8631cb084c6e112bcc3cc12d6ed7a7a484bd938
Francesco Bergesio <francesco.bergesi@edu.unito.it>
2019-03-29 14:22:31 +0100

    bug fixes

--- Prolog/dfs_limitata/script.pl
+++ Prolog/dfs_limitata/script.pl
@@ -2,13 +2,15 @@
 
 main :- consult(['././dfs_limitata/dfs_limitata.pl']),
   consult(['././dfs_limitata/../azioni.pl']),
-  consult(['././dfs_limitata/../data/esempio9a.pl']),
+  consult(['././dfs_limitata/../data/esempio4a.pl']),
   depth_limit_search(X, 30),
   atomic_list_concat(X, ', ', A),
   atom_string(A, S),
-  format("Solution = "),  format(S),
+  format("Solution = "),
+  format(S),
   format(".\n"),
   halt.
 
-main :- format("Solution = "),  format("false.\n"),
+main :- format("Solution = "),
+  format("false.\n"),
   halt.
--- Prolog/iterative_deepening/script.pl
+++ Prolog/iterative_deepening/script.pl
@@ -2,13 +2,15 @@
 
 main :- consult(['././iterative_deepening/iterative_deepening.pl']),
   consult(['././iterative_deepening/../azioni.pl']),
-  consult(['././iterative_deepening/../data/esempio9a.pl']),
+  consult(['././iterative_deepening/../data/esempio4a.pl']),
   iterative_deepening(X, 30),
   atomic_list_concat(X, ', ', A),
   atom_string(A, S),
-  format("Solution = "),  format(S),
+  format("Solution = "),
+  format(S),
   format(".\n"),
   halt.
 
-main :- format("Solution = "),  format("false.\n"),
+main :- format("Solution = "),
+  format("false.\n"),
   halt.
