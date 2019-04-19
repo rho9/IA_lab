9ab1a0c67394e7267878a7f5d8a560aac2354ffa
Francesco Bergesio <francesco.bergesi@edu.unito.it>
2019-04-19 14:56:00 +0200

    add fixes

--- Prolog/ida_star/ida_star.pl
+++ Prolog/ida_star/ida_star.pl
@@ -16,37 +16,37 @@ ida_star_rec(Result, _, _, _, _) :- %caso base
 
 ida_star_rec(Result, Bound, S, Path, Visited) :-
   heuristic(H, S),!,
-  search(Result, 0, H, Bound, S, Path, Visited),
-  ida_star_rec(Result, Result, S, Path, Visited).  
+  Min is inf,
+  search(NewResult, Min, 0, H, Bound, S, Path, Visited, Res),
+  ida_star_rec(NewResult, NewResult, S, Path, Visited).  
 
 %perché altimenti se fallisce il dopo potrebbe rifare un'altro caso base
-search(Result, _, F, Bound, _, _, _) :-
+search(Result, _, _, F, Bound, _, _, _, NewResult) :-
   F > Bound,!,
-  Result = F.
+  write(NewResult),
+  write(" is "),
+  write(F),
+  write("\n"),
+  NewResult is F.
   
-search(Result, _, _, _, S, _, _) :-
+search(Result, _, _, _, _, S, _, _, NewResult) :-
   finale(S),!,
-  Result = -1. %found
+  NewResult = -1. %found
 
-search(Result, G, _, Bound, S, [Action|Actions], Visited) :-
-  Min = inf,
+search(Result, Min, G, _, Bound, S, [Action|Actions], Visited, NewResult) :-
   applicabile(Action,S),
   trasforma(Action,S,SNuovo),
   \+member(SNuovo,Visited),
   NewG is G + 1,
   heuristic(H, SNuovo),
   NewF is NewG + H,
-  search(Result, NewG, NewF, Bound, SNuovo, Actions, [SNuovo|Visited]),
-  updateMin(Min, Result).
+  search(NewResult, Min, NewG, NewF, Bound, SNuovo, Actions, [SNuovo|Visited], Res),
+  updateMin(Min, NewResult, NewMin),
+  NewResult is NewMin.
 
-updateMin(Min, Result) :- 
-  format("ciao1"),
-  \+(Result == -1),
-  format("ciao2"),!,
-  format("ciao3"),
-  Min is Result,
-  format("ciao4"),
-  assert(Min).
+updateMin(Min, Result, NewMin) :- 
+  Min >= Result,!,
+  NewMin is Result.
 
 /*% caso base (non c'è soluzione, tutte le euristiche sono peggiori di quella di partenza)
 search([ActualNode|Path], G, Bound, Result) :-
@@ -67,43 +67,3 @@ search([ActualNode|Path], G, Bound, Result) :-
   \+finale(ActualNode),
   Min is inf, % ci si augura che funzioni
  */ 
-
-/*
- path              current search path (acts like a stack)
- node              current node (last node in current path)
- g                 the cost to reach current node
- f                 estimated cost of the cheapest path (root..node..goal)
- h(node)           estimated cost of the cheapest path (node..goal)
- cost(node, succ)  step cost function
- is_goal(node)     goal test
- successors(node)  node expanding function, expand nodes ordered by g + h(node)
- ida_star(root)    return either NOT_FOUND or a pair with the best path and its cost
- 
- procedure ida_star(root)
-   bound := h(root)
-   path := [root]
-   loop
-     t := search(path, 0, bound)
-     if t = FOUND then return (path, bound)
-     if t = ∞ then return NOT_FOUND
-     bound := t
-   end loop
- end procedure
- 
- function search(path, g, bound)
-   node := path.last
-   f := g + h(node)
-   if f > bound then return f
-   if is_goal(node) then return FOUND
-   min := ∞
-   for succ in successors(node) do
-     if succ not in path then
-       path.push(succ)
-       t := search(path, g + cost(node, succ), bound)
-       if t = FOUND then return FOUND
-       if t < min then min := t
-       path.pop()
-     end if
-   end for
-   return min
- end function*/
