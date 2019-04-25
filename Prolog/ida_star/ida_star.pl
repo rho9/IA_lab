f05557e617b3419ea59742c034aa356d79260613
Francesco Bergesio <francesco.bergesi@edu.unito.it>
2019-04-25 17:02:55 +0200

    add working retract

--- Prolog/ida_star/ida_star.pl
+++ Prolog/ida_star/ida_star.pl
@@ -16,46 +16,39 @@ ida_star_rec(Result, _, _, _, _) :- %caso base
 
 ida_star_rec(Result, Bound, S, Path, Visited) :-
   heuristic(H, S),!,
-  Min is inf,
-  search(NewResult, Min, 0, H, Bound, S, Path, Visited, Res),
+  assert(variables(min, inf)),
+  search(NewResult, 0, H, Bound, S, Path, Visited),
   ida_star_rec(NewResult, Res, S, Path, Visited).  
 
 %perché altimenti se fallisce il dopo potrebbe rifare un'altro caso base
-search(Result, _, _, F, Bound, _, _, _, NewResult) :-
+search(Result, _, F, Bound, _, _, _) :-
   F > Bound,!,
   Result is F.
   
-search(Result, _, _, _, _, S, _, _, NewResult) :-
+search(Result, _, _, _, S, _, _) :-
   finale(S),!,
   NewResult = -1. %found
 
-search(Result, Min, G, _, Bound, S, [Action|Actions], Visited, NewResult) :-
+search(Result, G, _, Bound, S, [Action|Actions], Visited) :-
   applicabile(Action,S),
   trasforma(Action,S,SNuovo),
   \+member(SNuovo,Visited),
   NewG is G + 1,
   heuristic(H, SNuovo),
   NewF is NewG + H,
-  search(NewResult, Min, NewG, NewF, Bound, SNuovo, Actions, [SNuovo|Visited], Res),
-  updateMin(Min, NewResult),
-  write("prima dell'assegnamento in search"),
-  NewResult = Atom_min,
-  write("prima dell'assegnamento in search").
+  search(NewResult, NewG, NewF, Bound, SNuovo, Actions, [SNuovo|Visited]),
+  updateMin(NewResult),
+  variables(min, NewMin),
+  writeln(NewMin),
+  Result = NewMin.
   
-updateMin(Min, Result) :-
-  writeln(updateMin1),
+updateMin(Result) :-
+  variables(min, Min),
   Min >= Result,!,
-  writeln(updateMin2),
-  atom_number(Atom_min, Min), % devo usare il predicato atom_number perché retract si aspetta un atomo e gli interi non lo sono
-  writeln(updateMin3),
-  retract(Atom_min), % Atom_min è un atomo, eppure retract non funziona
-  writeln(updateMin4),
-  Atom_min = Result,
-  writeln(updateMin5),
-  assert(Atom_min),
-  writeln(updateMin6),
+  retract(variables(min, Min)),
+  assert(variables(min, Result)).
 
-updateMin(_,_,_). % non devo aggiornare -> mantengo il vecchio valore di min
+updateMin(_). % non devo aggiornare -> mantengo il vecchio valore di min
   
 /*% caso base (non c'è soluzione, tutte le euristiche sono peggiori di quella di partenza)
 search([ActualNode|Path], G, Bound, Result) :-
