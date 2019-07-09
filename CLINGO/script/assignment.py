f25160b224fafdd262dfa82e7b9fe39b4112ec6e
Francesco Bergesio <francesco.bergesi@edu.unito.it>
2019-07-09 12:02:48 +0200

    added test script clingo and updated .gitignore

---
+++ CLINGO/script/assignment.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from sys import argv
+import re
+import copy
+
+f = open(argv[1], "r")
+content = f.read()
+
+giorni = ["lun", "mar", "mer", "gio", "ven"]
+ore = ["otto_nove","nove_dieci", "dieci_undici", "undici_dodici", "dodici_tredici", "tredici_quattordici", "quattordici_quindici"]
+aule = ["aula_lettere", "aula_matematica", "aula_tecnologia", "aula_musica", "aula_inglese", "aula_spagnolo", "aula_religione", "lab_arte", "lab_scienze", "lab_educazione_fisica"]
+
+orari = {}
+for i in range(len(aule)):
+    orari.update({aule[i]:{}})
+    for j in range(len(giorni)):
+        orari[aule[i]].update({giorni[j]:{}})
+        for k in range(len(giorni)):
+            orari[aule[i]][giorni[j]].update({ore[k]:""})
+content = content.replace('\n','')
+content = content.split(" ")
+#print(content)
+for line in content:
+    result = re.search(r'ore_a_settimana\(\w+,(\w+),(\w+),(\w+),(\w+)\)', line)
+    if result is not None: 
+        orari[result.group(1)][result.group(3)].update({result.group(2):result.group(4)})
+s=''
+for i in orari.keys():
+    print("\n" + i)
+    for j in orari[i].keys():
+        s+=j+','
+    s = s[:-1]
+    print(s)
+    s=''
+    for k in orari[i][j].keys():
+        print(k, end=',')
+        for j in orari[i].keys():
+            if orari[i][j].keys().__contains__(k):
+                s += orari[i][j][k]+','
+            else:
+                s += ','
+        s = s[:-1]
+        print(s)
+        s=''
