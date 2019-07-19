9da654c8328a94ff9620b896dc66f2a1b618fb04
Francesco Bergesio <francesco.bergesi@edu.unito.it>
2019-07-19 09:49:27 +0200

    revert repo to old one

--- Prolog/ida_star/ida_star.1.pl
+++ Prolog/ida_star/ida_star.1.pl
@@ -1,60 +1,43 @@
 ida_star(Path, Actions):- %cose che devono esssere istanziate
   iniziale(Start),
-  heuristic(Bound, Start),
-  nb_setval(global_bound, Bound),
-  ida_star_rec(Result, 0, Start, [Start], Actions).
+  heuristic(H, Start),
+  ida_star_rec(Result, H, 0, Start, [Start], Actions).
 
-% forse Result non serve: ho finito quando le azioni sono vuote e sono in finale
-ida_star_rec(_,_,ActualNode,_,[]) :-
-  finale(ActualNode).
+ida_star_rec(Result,_,_,_,_,_) :-
+  integer(Result).
 
-ida_star_rec(Result, G, ActualNode, Path, Actions) :-
-	nb_getval(global_bound, Bound),
+ida_star_rec(Result, Bound, G, Node, Path, Actions):-
   nb_setval(minF, inf),
-	search(Result, Bound, G, ActualNode, Path, Actions),!.
+  !, %svuoto lo stack del backtraking
+  \+search(Result, Bound, G, Node, Path, Actions),
+  nb_getval(minF, Min),
+  ida_star_rec(Result, Min, G, Node, Path, Actions).
 
-% aggiungere altro ida_star_rec che aggiorna il bound a minF
-ida_star_rec(Result, G, ActualNode, Path, Actions) :-
-    nb_getval(minF, NewBound),
-    nb_setval(global_bound, NewBound),
-    nb_setval(minF, inf),
-    search(Result, NewBound, G, ActualNode, Path, Actions),!.
+% raggiunto il nodo finale
+search(Result,_,_,Node,_,_):-
+  finale(Node),
+  Result is -1.
 
-
-/*
-VARI CASI DI SEARCH:
-1) soluz trovata
-2) superiamo bound ma abbiamo ancora azioni da provare
-3) superiamo il bound ma non abbiamo più azioni da provare
-4) non superiamo il bound.
-Continuare da sotto.
-*/
-
-
-
-% soluzione trovata
-search(_,_,_,ActualNode,_,[]) :-
-  finale(ActualNode).
-
-% F supera il bound
-search(Result, Bound, G, ActualNode, Path, Actions) :-
-  heuristic(H, ActualNode),
-  F is H+G,
-  F > Bound,
-  nb_getval(minF, FOld),
-  FOld>F,
-  nb_setval(minF, F),
+% quando il bound è da aggiornare
+search(_,Bound, G, Node, _, _) :-
+  heuristic(H, Node),
+  G+H > Bound,
+  nb_getval(minF, Min),
+  Min > G+H,
+  nb_setval(minF, G+H),
   false.
   % devo ritornare f. così cosa fa?:
   %search(Result, NewBound, G, ActualNode, Path, Actions).
 
-search(Result, Bound, G, ActualNode, Path, [Action|Actions]) :-
-	applicabile(Action, ActualNode),
-	trasforma(Action, ActualNode, NewNode),
-	\+member(NewNode, Path),
-	NewG is G+1,
+
+% raggiunto quando il bound non è da aggiornare
+search(Result,Bound,G,Node,Path,[Action|Actions]):-
+  applicabile(Action, Node),
+  trasforma(Action, Node, NewNode),
+  \+member(NewNode, Path), % controlla di non esserci già passato
+  heuristic(H, Node),
+  G+H =< Bound,
   heuristic(NewH, NewNode),
-  F is H+G,
-  F<=Bound,
-	search(Result, Bound, NewG, NewNode, [NewNode|Path], Actions),
-	false.
+  New_G is G+1,
+  F is New_G+NewH,
+  search(Result,Bound, New_G, NewNode,[NewNode|Path], Actions),!.
