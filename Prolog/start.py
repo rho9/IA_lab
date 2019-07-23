1560860eb6659c2fa60994f34761bbbe5c967a53
umbynos <umberto.baldi@edu.unito.it>
2019-07-23 14:47:21 +0200

    add feature: pass labirinth as param

--- Prolog/start.py
+++ Prolog/start.py
@@ -5,9 +5,16 @@ import sys
 
 path = sys.argv[0][:0-len("start.py")]
 
+
+if len(sys.argv) < 2:
+    print("usage python3 start.py labirinth_name")
+    exit()
+
+labirinth_name = sys.argv[1]
+
 print("\nStarting ida star ...")
-sub.run(["./"+path+"ida_star/load.py", "labirinto_easy", "100"])
+sub.run(["./"+path+"ida_star/load.py", labirinth_name, "100"])
 print("\nStarting iterative deepening ...")
-sub.run(["./"+path+"iterative_deepening/load.py", "labirinto_easy", "100"])
+sub.run(["./"+path+"iterative_deepening/load.py", labirinth_name, "100"])
 print("\nStarting a star ...")
-sub.run(["./"+path+"a_star/load.py", "labirinto_easy", "100"])
+sub.run(["./"+path+"a_star/load.py", labirinth_name, "100"])
