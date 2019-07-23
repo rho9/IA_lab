1aafa20e8206e1257a1f4428335c197ef4aa0d44
umbynos <umberto.baldi@edu.unito.it>
2019-07-23 14:47:06 +0200

    add some examples of labirinths

---
+++ Prolog/data/labirinto_con_sol_vuoto_20.pl
@@ -0,0 +1,12 @@
+% Configurazione labirinto labirinto_con_sol_vuoto_20
+
+num_righe(20).
+num_colonne(20).
+
+iniziale(pos(20, 20)).
+finale(pos(1, 1)).
+
+occupata(pos(10, 10)).
+occupata(pos(11, 10)).
+occupata(pos(10, 11)).
+occupata(pos(11, 11)).
---
+++ Prolog/data/labirinto_no_sol_10.pl
@@ -0,0 +1,52 @@
+% Configurazione labirinto prova
+
+num_righe(10).
+num_colonne(10).
+
+iniziale(pos(6, 3)).
+finale(pos(8, 9)).
+
+occupata(pos(2, 1)).
+occupata(pos(6, 1)).
+occupata(pos(9, 1)).
+occupata(pos(2, 2)).
+occupata(pos(3, 2)).
+occupata(pos(5, 2)).
+occupata(pos(7, 2)).
+occupata(pos(8, 2)).
+occupata(pos(10, 2)).
+occupata(pos(1, 3)).
+occupata(pos(2, 3)).
+occupata(pos(3, 3)).
+occupata(pos(4, 3)).
+occupata(pos(4, 4)).
+occupata(pos(5, 4)).
+occupata(pos(2, 5)).
+occupata(pos(4, 5)).
+occupata(pos(5, 5)).
+occupata(pos(6, 5)).
+occupata(pos(1, 6)).
+occupata(pos(2, 6)).
+occupata(pos(4, 6)).
+occupata(pos(5, 6)).
+occupata(pos(6, 6)).
+occupata(pos(7, 6)).
+occupata(pos(8, 6)).
+occupata(pos(5, 7)).
+occupata(pos(7, 7)).
+occupata(pos(8, 7)).
+occupata(pos(10, 7)).
+occupata(pos(1, 8)).
+occupata(pos(3, 8)).
+occupata(pos(7, 8)).
+occupata(pos(8, 8)).
+occupata(pos(9, 8)).
+occupata(pos(10, 8)).
+occupata(pos(1, 9)).
+occupata(pos(3, 9)).
+occupata(pos(9, 9)).
+occupata(pos(10, 9)).
+occupata(pos(1, 10)).
+occupata(pos(2, 10)).
+occupata(pos(5, 10)).
+occupata(pos(10, 10)).
