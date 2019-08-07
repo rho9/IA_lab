41122d879f909724dae164398c94677fb9bac98b
Rosita Gerbo <rosita.gerbo@edu.unito.it>
2019-08-07 17:35:50 +0200

    add action cost and change some comments

--- Prolog/ida_star/ida_star.pl
+++ Prolog/ida_star/ida_star.pl
@@ -5,10 +5,10 @@
 
 % ida_star(Actions)
 % Actions: actions from initial to final node
-ida_star(Actions):-
+ida_star(Actions, ActionCost):-
   iniziale(Start),
   heuristic(H, Start),
-  ida_star_rec(_, H, 0, Start, [Start], Actions).
+  ida_star_rec(_, H, 0, Start, [Start], Actions, ActionCost).
 
 
 % ida_star_rec(Result, Bound, G, Node, Path, Actions)
@@ -18,20 +18,20 @@ ida_star(Actions):-
 % Node: actual node
 % Path: list of nodes from initial node to Node
 % Actions: list of actions that bring from initial node to Node
-ida_star_rec(Result,  _, _, _, _, _):-
+ida_star_rec(Result,  _, _, _, _, _, _):-
   nonvar(Result), !. % True if Result is not a free variable.
 
 % search rule fails. If minF has infinite as value, no solutions is possible
-ida_star_rec(Result, Bound, G, Node, Path, Actions):-
+ida_star_rec(Result, Bound, G, Node, Path, Actions, ActionCost):-
   nb_setval(minF, inf), % minF: global variable in which is saved the best bound (the best F)
-  \+search(Result, Bound, G, Node, Path, Actions),
+  \+search(Result, Bound, G, Node, Path, Actions, ActionCost),
   nb_getval(minF, Min),
-  Min \== inf, % non c'è soluzione: search ha fallito -> non ho più nodi e minF è a infinito. Richiama quello sotto, ma tanto fallirà pure lei
-  ida_star_rec(Result, Min, G, Node, Path, Actions).
+  Min \== inf,
+  ida_star_rec(Result, Min, G, Node, Path, Actions, ActionCost).
 
-ida_star_rec(Result, Bound, G, Node, Path, Actions):-
-  search(Result, Bound, G, Node, Path, Actions),
-  ida_star_rec(Result, Bound, G, Node, Path, Actions).
+ida_star_rec(Result, Bound, G, Node, Path, Actions, ActionCost):-
+  search(Result, Bound, G, Node, Path, Actions, ActionCost),
+  ida_star_rec(Result, Bound, G, Node, Path, Actions, ActionCost).
 
 
 % search(Result, Bound, G, Node, Path, Actions)
@@ -41,13 +41,13 @@ ida_star_rec(Result, Bound, G, Node, Path, Actions):-
 % Node: actual node
 % Path: list of nodes from initial node to Node
 % Actions: list of actions that bring from initial node to Node
-search(Result, _, _, Node, _, Actions):-
+search(Result, _, _, Node, _, Actions, _):-
   finale(Node),
   finale(Result),
   Actions = []. % avoid that a free variable is added to the solution
 
 % the bound must be updated
-search(_, Bound, G, Node, _, _) :-
+search(_, Bound, G, Node, _, _, _) :-
   heuristic(H, Node),
   G+H > Bound,
   nb_getval(minF, Min),
@@ -56,11 +56,11 @@ search(_, Bound, G, Node, _, _) :-
   false.
 
 % the bound must not be updated
-search(Result, Bound, G, Node, Path, [Action|Actions]):-
+search(Result, Bound, G, Node, Path, [Action|Actions], ActionCost):-
   heuristic(H, Node),
   G+H =< Bound,
   applicabile(Action, Node),
   trasforma(Action, Node, NewNode),
   \+member(NewNode, Path),
-  New_G is G+1,
-  search(Result, Bound, New_G, NewNode, [NewNode|Path], Actions), !.
+  New_G is G+ActionCost,
+  search(Result, Bound, New_G, NewNode, [NewNode|Path], Actions, ActionCost), !.
