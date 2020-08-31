3eaacce6ec8a91ca30e7df05177ddeb2a20b9db7
Francesco Bergesio <francesco.bergesi@edu.unito.it>
2020-08-31 15:10:29 +0200

    eclipse files

--- Prolog/iterative_deepening/script.pl
+++ Prolog/iterative_deepening/script.pl
@@ -2,7 +2,7 @@
 
 main :- consult(['././iterative_deepening/iterative_deepening.pl']),
   consult(['././iterative_deepening/../azioni.pl']),
-  consult(['././iterative_deepening/../data/labirinto_no_sol_10.pl']),
+  consult(['././iterative_deepening/../data/labirinto_con_sol_30.pl']),
   consult(['././iterative_deepening/../heuristics/manhattan_distance.pl']),
   iterative_deepening(X, 15, 1),
   atomic_list_concat(X, ', ', A),
